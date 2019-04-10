c662e8c8207a3c474e8774e75f1e4bb57b7e33a9
Tyler Gasperlin <TylerG@mossadams.ma>
2019-04-10 11:28:40 -0700

    add db and controllers

---
+++ HarvestApp2/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HarvestApp2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HarvestApp2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public DbSet<Value> Values { get; set; }
+        
+       
+    }
+}
